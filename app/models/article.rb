44af3611dcbe80ae748157e7bd5820c44e1ff142
Issa Soumare <soumare.iss@gmail.com>
2016-05-24 10:43:48 +0200

    Verification image

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ActiveRecord::Base
   belongs_to :categorie
   has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  validates_with AttachmentSizeValidator,
+     attributes: :photo,
+     less_than: 1.megabytes
+  do_not_validate_attachment_file_type :photo
 end
